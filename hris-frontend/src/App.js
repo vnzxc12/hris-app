60c960a086f096c026030d2f935d2f80c86d296b
Vonn <vcserrano12@gmail.com>
2025-07-28 20:47:53 +0800

    updated all

--- hris-frontend/src/App.js
+++ hris-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from "./Dashboard";
 import EmployeeDetail from "./EmployeeDetails";
 import Unauthorized from "./Unauthorized"; 
@@ -8,14 +8,14 @@ import Login from "./Login";
 
 function App() {
   return (
-    <Router>
+    
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/employee/:id" element={<EmployeeDetail />} />
+        <Route path="/employee/:id" element={<EmployeeDetails />} />
         <Route path="/unauthorized" element={<Unauthorized />} />
       </Routes>
-    </Router>
+   
   );
 }
 
